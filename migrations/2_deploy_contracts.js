4d48d75ca758e4ba405a9c8ee26af630ba5fc93c
7emoji <emoji-engineering@gmail.com>
2020-09-19 17:43:32 +0200

    add deployment to local test-net

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,40 @@
-module.exports = function(deployer) {};
+//module.exports = function(deployer) {};
 
-// TODO: deployment depending on network type
+const EmojiToken = artifacts.require("EmojiToken");
+const MasterChef = artifacts.require("MasterChef");
+const EmojiBar = artifacts.require("EmojiBar");
+
+
+const UniswapFactory = artifacts.require("UniSwapV2Factory");
+const UniswapRouter = artifacts.require("UniswapV2Router02");
+const MockERC20 = artifacts.require("MockERC20");
+
+const EmojiMaker = artifacts.require("EmojiMaker");
+
+const migration = async function(deployer,network,accounts) {
+    await Promise.all([
+        deployToken(deployer,network,accounts),
+    ]);
+};
+
+module.exports = migration;
+
+async function deployToken(deployer,network,accounts) {
+
+    if( network === 'test' ) {
+        
+        dev_addr = accounts[9];
+
+        await deployer.deploy(EmojiToken);
+        await deployer.deploy(MasterChef, EmojiToken.address, dev_addr, 25, 0, 0);
+        await deployer.deploy(EmojiBar, EmojiToken.address);
+
+
+        await deployer.deploy(MockERC20, "Wrapped Ether", "WETH", 1000);
+        await deployer.deploy(UniswapFactory, dev_addr);
+        await deployer.deploy(UniswapRouter, UniswapFactory.address, dev_addr);
+
+        await deployer.deploy(EmojiMaker, UniswapFactory.address, EmojiBar.address, EmojiToken.address, MockERC20.address);
+    }
+
+}
