cfaf5f015bdc24110a2e1ea4e65a6342192b2d92
7emoji <emoji-engineering@gmail.com>
2020-10-11 15:34:53 +0200

    add deployment to ropsten network

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
+
 const EmojiToken = artifacts.require("EmojiToken");
 const MasterChef = artifacts.require("MasterChef");
 const EmojiBar = artifacts.require("EmojiBar");
-//const EmojiMaker = artifacts.require("EmojiMaker");
+const EmojiMaker = artifacts.require("EmojiMaker");
 
 const UniswapFactory = artifacts.require("UniSwapV2Factory");
 const UniswapRouter = artifacts.require("UniswapV2Router02");
 
-//const MockERC20 = artifacts.require("MockERC20");
+const Migrator = artifacts.require('Migrator');
+const WETH = artifacts.require('WETH');
+
 const Token0 = artifacts.require("Token0")
 const Token1 = artifacts.require("Token1")
-
 
 const migration = async function(deployer,network,accounts) {
     await Promise.all([
@@ -23,7 +26,7 @@ async function deployToken(deployer,network,accounts) {
 
     if( network === 'test' || network === 'develop') {
         
-        dev_addr = accounts[9];
+        let dev_addr = accounts[9];
         
         await deployer.deploy(EmojiToken);
         await deployer.deploy(MasterChef, EmojiToken.address, dev_addr, 25, 0, 0);
@@ -32,12 +35,50 @@ async function deployToken(deployer,network,accounts) {
         await deployer.deploy(UniswapFactory, dev_addr);
         await deployer.deploy(UniswapRouter, UniswapFactory.address, dev_addr);
   
+        // Two dummy tokens to test pairing
         await deployer.deploy(Token0, "AAA", "AAA", 100000000 );
         await deployer.deploy(Token1, "ZZZ", "ZZZ", 100000000 );
 
-        //await deployer.deploy(EmojiMaker, 
-        //    UniswapFactory.address, EmojiBar.address, EmojiToken.address, weth_address);
-  
+    }
+    else if ( network === 'ropsten' ) {
+
+        let dev_addr = process.env.DEV_ADDR;
+
+        await deployer.deploy(WETH);
+        const weth = await WETH.deployed();
+
+        let weth_addr = weth.address;
+        
+        const emojiToken = await deployer.deploy(EmojiToken);
+        await deployer.deploy(MasterChef, EmojiToken.address, dev_addr, 25, 0, 0);
+        await deployer.deploy(EmojiBar, EmojiToken.address);
+        const factory = await deployer.deploy(UniswapFactory, dev_addr);
+
+        const poolToken = await factory.createPair( weth_addr, emojiToken.address );
+        console.log("pool: ", poolToken);
+
+        await deployer.deploy(UniswapRouter, factory.address, weth_addr);
+
+        
+        const masterChef = await MasterChef.deployed();
+        // MasterChef is the master of Emoji. He can make Emoji and he is a fair guy.
+        await emojiToken.transferOwnership(masterChef.address);
+
+        await deployer.deploy(EmojiMaker,
+             UniswapFactory.address, EmojiBar.address, EmojiToken.address, weth_addr);
+        
+        const emojiMaker = await EmojiMaker.deployed();
+        await factory.setFeeTo(emojiMaker.address);
+
+
+        await deployer.deploy(
+            Migrator,
+            masterChef.address,
+            // https://ropsten.etherscan.io/address/0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f 
+            '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+            factory.address,
+            1
+        );
     }
 
 }
